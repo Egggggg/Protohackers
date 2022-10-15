c098d2e9ebb84b85fcf7eef684238b9dfcfda03e
Egggggg <mrhumanman2@gmail.com>
2022-10-15 19:14:57 -0400

    Finished 1 :crab:

--- 1/src/main.rs
+++ 1/src/main.rs
@@ -7,16 +7,16 @@ use tokio::{
 #[derive(Deserialize, Debug)]
 struct Transfer {
     method: String,
-    number: f32,
+    number: f64,
 }
 
-const MALFORMED: &'static [u8; 39] = b"{\"method\": \"malformed\", \"prime\": false}";
-const PRIME: &'static [u8; 36] = b"{\"method\": \"isPrime\", \"prime\": true}";
-const NOT_PRIME: &'static [u8; 37] = b"{\"method\": \"isPrime\", \"prime\": false}";
+const MALFORMED: &'static [u8; 40] = b"{\"method\": \"malformed\", \"prime\": false}\n";
+const PRIME: &'static [u8; 37] = b"{\"method\": \"isPrime\", \"prime\": true}\n";
+const NOT_PRIME: &'static [u8; 38] = b"{\"method\": \"isPrime\", \"prime\": false}\n";
 
 #[tokio::main]
 async fn main() {
-    let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
+    let listener = TcpListener::bind("0.0.0.0:8080").await.unwrap();
 
     loop {
         let (mut socket, addr) = listener.accept().await.unwrap();
@@ -35,7 +35,17 @@ async fn process(socket: &mut TcpStream) {
     let buf_reader = BufReader::new(reader);
     let mut lines = buf_reader.lines();
 
-    while let Some(line) = lines.next_line().await.unwrap() {
+    loop {
+        println!("loop");
+
+        let line = lines.next_line().await.unwrap_or(None);
+
+        if line.is_none() {
+            return;
+        }
+
+        let line = line.unwrap();
+
         println!("{}", line);
 
         let input: Result<Transfer, _> = serde_json::from_str(&line);
@@ -45,38 +55,62 @@ async fn process(socket: &mut TcpStream) {
                 println!("{:?}", input);
 
                 if input.method != "isPrime" {
+                    println!("malformed");
                     writer.write_all(MALFORMED).await.unwrap();
                     return;
                 }
 
                 if input.number.trunc() != input.number {
+                    println!("trunc: {}, normal: {}", input.number.trunc(), input.number);
+                    println!("not prime");
                     writer.write_all(NOT_PRIME).await.unwrap();
                     continue;
                 }
 
-                let abs = input.number.abs();
-                let root = abs.sqrt();
+                let number = input.number;
+
+                println!("number: {}, input.number: {}", number, input.number);
+
+                if number < 2.0 {
+                    println!("not prime");
+                    writer.write_all(NOT_PRIME).await.unwrap();
+                    continue;
+                }
+
+                if number == 2.0 {
+                    println!("prime");
+                    writer.write_all(PRIME).await.unwrap();
+                    continue;
+                }
+
+                let root = number.sqrt();
                 let mut factor_found = false;
 
-                println!("abs: {}, root: {}", abs, root);
+                println!("number: {}, root: {}", number, root);
 
                 for i in 2..=root.ceil() as u32 {
-                    let result = abs / i as f32;
+                    let result = number / i as f64;
 
                     if result.trunc() == result {
+                        println!("{}: not prime", input.number);
                         writer.write_all(NOT_PRIME).await.unwrap();
                         factor_found = true;
                         break;
                     }
                 }
 
+                println!("factor found: {}", factor_found);
+
                 if !factor_found {
+                    println!("{}: prime", input.number);
                     writer.write_all(PRIME).await.unwrap();
+                    continue;
                 }
             }
             Err(err) => {
                 println!("{:?}", err);
 
+                println!("malformed");
                 writer.write_all(MALFORMED).await.unwrap();
 
                 return;
